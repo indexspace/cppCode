6c4510d48220d2eafc883f7207a647a0200216e0
indexspace <2838839917@qq.com>
2023-10-04 17:20:13 +0800

    10.4

--- Test.cpp
+++ Test.cpp
@@ -1,20 +1,33 @@
 #include<iostream>
 using namespace std;
 
-int maxVal = INT_MIN;
-int n;
+const int N = 1000;
+long num[N][N] = {0};
+long dp[N][N] = {0};
+int r;
  
  int main(){
- 	
- 	while(~scanf("%d", &n)){
- 		if(n == 0) {
-			cout << maxVal << endl;
-			maxVal = INT_MIN; 	
-		}
-		else if(n > maxVal){
-			maxVal = n;
+
+	cout << "Çë¼üÈër = ";
+	cin >>  r;
+	cout << "Çë¼üÈëÊýËþ" << endl;;
+	for(int i = 0; i < r; i++) {
+		for(int j = 0; j <= i; j++) {
+			cin >> num[i][j];
 		}
 	}
+
+	
+	for(int i = 0; i < r; i++) {
+		dp[r-1][i] = num[r-1][i];
+	}
+	for(int i = r-2; i >= 0; i--) {
+		for(int j = 0; j <= i; j++) {
+			dp[i][j] = num[i][j] + max(dp[i+1][j],dp[i+1][j+1]);
+		}
+	}
+
+	cout << dp[0][0] << endl;
  	
  	return 0;
  }
